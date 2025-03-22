6fe1af4d3bbd7451126b465eec9bf9e534d72c55
Yuhan Chen <641481313@qq.com>
2025-03-23 10:05:43 +1100

    test

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/')
 def home():
-    return "Hello, Test!"
+    return "Helloddddddddddddddddddddddddddddddddddddddddddd"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
